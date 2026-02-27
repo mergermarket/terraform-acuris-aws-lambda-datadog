4dbb5d825ef3989a93a722fd670d0d884a2c986b
Keir Badger <keir.badger@iongroup.com>
2026-02-27 15:40:04 +0000

    revert prev changes

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "lambda_arn" {
-  value = module.lambda-datadog.lambda_arn
+  value = module.lambda-datadog.aws_lambda_function.this.arn
 }
 
 output "lambda_function_name" {
-  value = module.lambda-datadog.lambda_function_name
+  value = module.lambda-datadog.aws_lambda_function.this.function_name
 }
 
 output "lambda_iam_role_name" {
   value = aws_iam_role.iam_for_lambda.name
 }
 
-# output "lambda_invoke_arn" {
-#   value = module.lambda-datadog.aws_lambda_function.this.invoke_arn
-# }
+output "lambda_invoke_arn" {
+  value = module.lambda-datadog.aws_lambda_function.this.invoke_arn
+}
