f9ddceb2ad537390c4ea59c424455629c47bb3fe
Keir Badger <keir.badger@iongroup.com>
2026-02-27 16:04:38 +0000

    try outputs again

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "lambda_arn" {
-  value = module.lambda-datadog.aws_lambda_function.this.arn
+  value = module.lambda-datadog.arn
 }
 
 output "lambda_function_name" {
-  value = module.lambda-datadog.aws_lambda_function.this.function_name
+  value = module.lambda-datadog.function_name
 }
 
 output "lambda_iam_role_name" {
@@ -11,5 +11,5 @@ output "lambda_iam_role_name" {
 }
 
 output "lambda_invoke_arn" {
-  value = module.lambda-datadog.aws_lambda_function.this.invoke_arn
+  value = module.lambda-datadog.invoke_arn
 }
